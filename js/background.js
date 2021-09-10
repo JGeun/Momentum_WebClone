9919fee3722f14d0323e85d396dbad9184604fdb
JGeun <pjk8926@gmail.com>
2021-09-10 17:14:05 +0900

    because git page image 404 error

--- js/background.js
+++ js/background.js
@@ -1,15 +1,15 @@
-const num = Math.floor(Math.random()*2);
-if(num == 0){
-    document.body.style.background = "url(../images/image0.jpg) no-repeat";
-}
-else{
+// const num = Math.floor(Math.random()*2);
+// if(num == 0){
+//     document.body.style.background = "url(../images/image0.jpg) no-repeat";
+// }
+// else{
     document.body.style.background = "url(../images/image1.jpg) no-repeat";
-}
+// }
 document.body.style.backgroundSize = "cover";
 document.body.style.backgroundPosition = "center";
 
-if(num == 0){
-    document.getElementById("quote").style.color = "black";
-}else{
+// if(num == 0){
+//     document.getElementById("quote").style.color = "black";
+// }else{
     document.getElementById("quote").style.color = "white";
-}
+// }
