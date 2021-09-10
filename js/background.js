0bfd91fa78c3399ff28da4b264b89ee585dacf60
JGeun <pjk8926@gmail.com>
2021-09-10 17:15:29 +0900

    fix

--- js/background.js
+++ js/background.js
@@ -3,7 +3,7 @@
 //     document.body.style.background = "url(../images/image0.jpg) no-repeat";
 // }
 // else{
-    document.body.style.background = "url(../images/image1.jpg) no-repeat";
+    // document.body.style.background = "url(../images/image1.jpg) no-repeat";
 // }
 document.body.style.backgroundSize = "cover";
 document.body.style.backgroundPosition = "center";
